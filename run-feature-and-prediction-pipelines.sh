2332902f140004919d334205b060f82b185bba57
amulyaprasanth <amulyaprasanth301@gmail.com>
2025-02-15 11:13:03 +0530

    added workflow to run the feature and prediction pipeline daily

---
+++ run-feature-and-prediction-pipelines.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Change directory to the module containing the Jupyter notebooks
+cd src/01-module
+
+# Execute the feature pipeline notebook and convert it to a notebook format
+jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
+
+# Execute the batch inference pipeline notebook and convert it to a notebook format
+jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
